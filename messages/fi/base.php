e769bff51f80f81d64df781d4e34e7864809215d
HumHub Translations <info@humhub.com>
2019-02-02 18:02:20 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/fi/base.php
+++ messages/fi/base.php
@@ -1,13 +1,12 @@
 <?php
-
-return [
-    '<strong>Create</strong> draw.io diagram' => '',
-    '<strong>Draw.io </strong> module configuration' => '',
-    'Could not find requested file!' => '',
-    'Create draw.io document' => '',
-    'Edit using draw.io' => '',
-    'File write access denied!' => '',
-    'Hostname' => '',
-    'Open the new document in the next step' => '',
-    'e.g. https://draw.io' => '',
-];
+return array (
+  '<strong>Create</strong> draw.io diagram' => '<strong>Luo</strong> draw.io tiedosto',
+  '<strong>Draw.io </strong> module configuration' => '<strong>Draw.io </strong> laajennuksen hallinta',
+  'Could not find requested file!' => 'Pyydettyä tiedostoa ei löytyynyt',
+  'Create draw.io document' => 'Luo draw.io tiedosto',
+  'Edit using draw.io' => 'Muokkaa draw.iolla',
+  'File write access denied!' => 'Tiedoston muokkaus estetty',
+  'Hostname' => 'Isäntänimi',
+  'Open the new document in the next step' => 'Avaa tiedosto heti',
+  'e.g. https://draw.io' => 'esim. https://draw.io',
+);
