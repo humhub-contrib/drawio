ab10b05dce866865913ab886a6e929c6322afc55
HumHub Translations <info@humhub.com>
2019-09-23 15:03:58 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/am/base.php
+++ messages/am/base.php
@@ -1,13 +1,12 @@
 <?php
-
-return [
-    '<strong>Create</strong> draw.io diagram' => '',
-    '<strong>Draw.io </strong> module configuration' => '',
-    'Could not find requested file!' => '',
-    'Create draw.io document' => '',
-    'Edit using draw.io' => '',
-    'File write access denied!' => '',
-    'Hostname' => '',
-    'Open the new document in the next step' => '',
-    'e.g. https://draw.io' => '',
-];
+return array (
+  '<strong>Create</strong> draw.io diagram' => '',
+  '<strong>Draw.io </strong> module configuration' => '',
+  'Could not find requested file!' => '',
+  'Create draw.io document' => '',
+  'Edit using draw.io' => '',
+  'File write access denied!' => '',
+  'Hostname' => 'የአስተናጋጅ ስም',
+  'Open the new document in the next step' => '',
+  'e.g. https://draw.io' => '',
+);
