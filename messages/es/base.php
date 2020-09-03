b93dce7d4921ba123bf20f903a71045199364771
HumHub Translations <info@humhub.com>
2020-09-03 10:42:54 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/es/base.php
+++ messages/es/base.php
@@ -1,12 +1,12 @@
 <?php
 return array (
-  '<strong>Create</strong> draw.io diagram' => '',
-  '<strong>Draw.io </strong> module configuration' => '',
+  '<strong>Create</strong> draw.io diagram' => '<strong>Crear</strong> diagrama draw.io',
+  '<strong>Draw.io </strong> module configuration' => 'Configuración del módulo <strong>Draw.io </strong>',
   'Could not find requested file!' => '¡No se pudo encontrar el archivo solicitado!',
-  'Create draw.io document' => '',
-  'Edit using draw.io' => '',
-  'File write access denied!' => '',
+  'Create draw.io document' => 'Crear documento draw.io',
+  'Edit using draw.io' => 'Editar usando draw.io',
+  'File write access denied!' => 'Acceso a escritura de archivos denegada!',
   'Hostname' => 'Nombre de servidor',
-  'Open the new document in the next step' => '',
-  'e.g. https://draw.io' => '',
+  'Open the new document in the next step' => 'Abrir el nuevo documento en el siguiente paso',
+  'e.g. https://draw.io' => 'por ejemplo: https://draw.io',
 );
