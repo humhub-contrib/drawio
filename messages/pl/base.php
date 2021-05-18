1052f34cfa6eac163af85a5d9d8ac4672f8ac53d
HumHub Translations <info@humhub.com>
2021-05-18 20:26:28 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/pl/base.php
+++ messages/pl/base.php
@@ -1,13 +1,12 @@
 <?php
-
-return [
-    '<strong>Create</strong> draw.io diagram' => '<strong>Utwórz</strong>diagram draw.io',
-    '<strong>Draw.io </strong> module configuration' => 'Konfiguracja modułu <strong>Draw.io </strong>',
-    'Could not find requested file!' => 'Nie znaleziono żądanego pliku!',
-    'Create draw.io document' => 'Utwórz dokument draw.io',
-    'Edit using draw.io' => 'Edytuj używając draw.io',
-    'File write access denied!' => 'Możliwość zapisu zablokowana!',
-    'Hostname' => 'Nazwa Hosta',
-    'Open the new document in the next step' => 'Otwórz nowy dokument w kolejnym kroku',
-    'e.g. :drawioDefaultServerUrl' => '',
-];
+return array (
+  '<strong>Create</strong> draw.io diagram' => '<strong>Utwórz</strong>diagram draw.io',
+  '<strong>Draw.io </strong> module configuration' => 'Konfiguracja modułu <strong>Draw.io </strong>',
+  'Could not find requested file!' => 'Nie znaleziono żądanego pliku!',
+  'Create draw.io document' => 'Utwórz dokument draw.io',
+  'Edit using draw.io' => 'Edytuj używając draw.io',
+  'File write access denied!' => 'Możliwość zapisu zablokowana!',
+  'Hostname' => 'Nazwa Hosta',
+  'Open the new document in the next step' => 'Otwórz nowy dokument w kolejnym kroku',
+  'e.g. :drawioDefaultServerUrl' => 'n.p. :domyslnyAdresSerweraDrawio',
+);
