02f7d89f66e71444564abdc76bf4d54761b41037
HumHub Translations <info@humhub.com>
2021-06-04 16:03:17 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/nl/base.php
+++ messages/nl/base.php
@@ -1,13 +1,12 @@
 <?php
-
-return [
-    '<strong>Create</strong> draw.io diagram' => '<strong>Maak</strong> een  draw.io diagram',
-    '<strong>Draw.io </strong> module configuration' => '<strong>Draw.io </strong> module configuratie',
-    'Could not find requested file!' => 'Kan het aangevraagde bestand niet vinden!',
-    'Create draw.io document' => 'Maak een draw.io document',
-    'Edit using draw.io' => 'Bewerken met draw.io',
-    'File write access denied!' => 'Schrijftoegang tot het bestand geweigerd.',
-    'Hostname' => 'Computernaam',
-    'Open the new document in the next step' => 'Open het nieuwe document in de volgende stap',
-    'e.g. :drawioDefaultServerUrl' => '',
-];
+return array (
+  '<strong>Create</strong> draw.io diagram' => '<strong>Maak</strong> een  draw.io diagram',
+  '<strong>Draw.io </strong> module configuration' => '<strong>Draw.io </strong> module configuratie',
+  'Could not find requested file!' => 'Kan het aangevraagde bestand niet vinden!',
+  'Create draw.io document' => 'Maak een draw.io document',
+  'Edit using draw.io' => 'Bewerken met draw.io',
+  'File write access denied!' => 'Schrijftoegang tot het bestand geweigerd.',
+  'Hostname' => 'Computernaam',
+  'Open the new document in the next step' => 'Open het nieuwe document in de volgende stap',
+  'e.g. :drawioDefaultServerUrl' => 'bijv. : drawioStandaardUrl',
+);
