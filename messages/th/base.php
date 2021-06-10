a69aa599c93a0e761ea89b347b572febacec9023
HumHub Translations <info@humhub.com>
2021-06-10 20:13:38 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/th/base.php
+++ messages/th/base.php
@@ -1,13 +1,12 @@
 <?php
-
-return [
-    '<strong>Create</strong> draw.io diagram' => '',
-    '<strong>Draw.io </strong> module configuration' => '',
-    'Could not find requested file!' => '',
-    'Create draw.io document' => '',
-    'Edit using draw.io' => '',
-    'File write access denied!' => '',
-    'Hostname' => '',
-    'Open the new document in the next step' => '',
-    'e.g. :drawioDefaultServerUrl' => '',
-];
+return array (
+  '<strong>Create</strong> draw.io diagram' => '<strong>สร้าง</strong> draw.io ไดอะแกรม',
+  '<strong>Draw.io </strong> module configuration' => '<strong>Draw.io </strong> การกำหนดค่าโมดูล',
+  'Could not find requested file!' => 'ไม่พบไฟล์ที่ร้องขอ!',
+  'Create draw.io document' => 'สร้างเอกสาร draw.io',
+  'Edit using draw.io' => 'แก้ไขโดยใช้ draw.io',
+  'File write access denied!' => 'การเข้าถึงการเขียนไฟล์ถูกปฏิเสธ!',
+  'Hostname' => 'ชื่อโฮสต์',
+  'Open the new document in the next step' => 'เปิดเอกสารใหม่ในขั้นตอนต่อไป',
+  'e.g. :drawioDefaultServerUrl' => 'เช่น :drawioDefaultServerUrl',
+);
