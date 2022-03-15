0024282f6bce046420e1bf558b66c0f011bd3cda
HumHub Translations <info@humhub.com>
2022-03-15 23:44:11 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/es/base.php
+++ messages/es/base.php
@@ -1,13 +1,12 @@
 <?php
-
-return [
-    '<strong>Create</strong> draw.io diagram' => '<strong>Crear</strong> diagrama draw.io',
-    '<strong>Draw.io </strong> module configuration' => 'Configuración del módulo <strong>Draw.io </strong>',
-    'Could not find requested file!' => '¡No se pudo encontrar el archivo solicitado!',
-    'Create draw.io document' => 'Crear documento draw.io',
-    'Edit using draw.io' => 'Editar usando draw.io',
-    'File write access denied!' => 'Acceso a escritura de archivos denegada!',
-    'Hostname' => 'Nombre de servidor',
-    'Open the new document in the next step' => 'Abrir el nuevo documento en el siguiente paso',
-    'e.g. :drawioDefaultServerUrl' => '',
-];
+return array (
+  '<strong>Create</strong> draw.io diagram' => '<strong>Crear</strong> diagrama draw.io',
+  '<strong>Draw.io </strong> module configuration' => 'Configuración del módulo <strong>Draw.io </strong>',
+  'Could not find requested file!' => '¡No se pudo encontrar el archivo solicitado!',
+  'Create draw.io document' => 'Crear documento draw.io',
+  'Edit using draw.io' => 'Editar usando draw.io',
+  'File write access denied!' => 'Acceso a escritura de archivos denegada!',
+  'Hostname' => 'Nombre de servidor',
+  'Open the new document in the next step' => 'Abrir el nuevo documento en el siguiente paso',
+  'e.g. :drawioDefaultServerUrl' => 'p. ej.: drawioDefaultServerUrl',
+);
--- messages/sv/base.php
+++ messages/sv/base.php
@@ -1,13 +1,12 @@
 <?php
-
-return [
-    '<strong>Create</strong> draw.io diagram' => '<strong>Skapa</strong> draw.io-diagram',
-    '<strong>Draw.io </strong> module configuration' => '<strong>Draw.io </strong> modul konfiguration',
-    'Could not find requested file!' => 'Den begärda filen hittades inte!',
-    'Create draw.io document' => 'Skapa ett draw.io dokument',
-    'Edit using draw.io' => 'Redigera genom att använda draw.io',
-    'File write access denied!' => 'Filens skrivåtkomst nekad!',
-    'Hostname' => 'Host namn',
-    'Open the new document in the next step' => 'Öppna det nya dokumentet i nästa steg',
-    'e.g. :drawioDefaultServerUrl' => '',
-];
+return array (
+  '<strong>Create</strong> draw.io diagram' => '<strong>Skapa</strong> draw.io-diagram',
+  '<strong>Draw.io </strong> module configuration' => '<strong>Draw.io </strong> modul konfiguration',
+  'Could not find requested file!' => 'Den begärda filen hittades inte!',
+  'Create draw.io document' => 'Skapa ett draw.io dokument',
+  'Edit using draw.io' => 'Redigera genom att använda draw.io',
+  'File write access denied!' => 'Filens skrivåtkomst nekad!',
+  'Hostname' => 'Host namn',
+  'Open the new document in the next step' => 'Öppna det nya dokumentet i nästa steg',
+  'e.g. :drawioDefaultServerUrl' => 't.ex. :drawioDefaultServerUrl',
+);
