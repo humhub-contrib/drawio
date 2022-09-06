510a47e3b0de1e3a74fce9a3f4ccd473f0575444
HumHub Translations <hello@humhub.com>
2022-09-06 16:32:35 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/pt/base.php
+++ messages/pt/base.php
@@ -1,13 +1,12 @@
 <?php
-
-return [
-    '<strong>Create</strong> draw.io diagram' => '<strong>Criar</strong> um diagrama draw.io',
-    '<strong>Draw.io </strong> module configuration' => '<strong>Configuração</strong> do módulo Draw.io',
-    'Could not find requested file!' => 'Não foi possível encontrar o ficheiro solicitado!',
-    'Create draw.io document' => 'Criar documento draw.io',
-    'Edit using draw.io' => 'Editar usando draw.io',
-    'File write access denied!' => 'Escrita de ficheiro recusada!',
-    'Hostname' => '<em>Hostname</em>',
-    'Open the new document in the next step' => 'Abre o novo documento no passo seguinte',
-    'e.g. :drawioDefaultServerUrl' => '',
-];
+return array (
+  '<strong>Create</strong> draw.io diagram' => '<strong>Criar</strong> um diagrama draw.io',
+  '<strong>Draw.io </strong> module configuration' => '<strong>Configuração</strong> do módulo Draw.io',
+  'Could not find requested file!' => 'Não foi possível encontrar o ficheiro solicitado!',
+  'Create draw.io document' => 'Criar documento draw.io',
+  'Edit using draw.io' => 'Editar usando draw.io',
+  'File write access denied!' => 'Escrita de ficheiro recusada!',
+  'Hostname' => '<em>Hostname</em>',
+  'Open the new document in the next step' => 'Abre o novo documento no passo seguinte',
+  'e.g. :drawioDefaultServerUrl' => 'e.g. :drawioDefaultServerUrl',
+);
