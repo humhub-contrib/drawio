eade7c1cfd393041f008aa0cfdfc2c8b01b219b2
HumHub Translations <hello@humhub.com>
2023-01-08 15:46:11 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/hu/base.php
+++ messages/hu/base.php
@@ -1,13 +1,12 @@
 <?php
-
-return [
-    '<strong>Create</strong> draw.io diagram' => '<strong>draw.io</strong> diagram létrehozása',
-    '<strong>Draw.io </strong> module configuration' => '<strong>Draw.io </strong> modul konfiguráció',
-    'Could not find requested file!' => 'Nem található a kért fájl!',
-    'Create draw.io document' => 'draw.io dokumentum létrehozása',
-    'Edit using draw.io' => 'Szerkesztés draw.io-val.',
-    'File write access denied!' => 'A fájl írása megtagadva!',
-    'Hostname' => 'Hosztnév',
-    'Open the new document in the next step' => 'Nyissa meg az új dokumentumot következő lépésként',
-    'e.g. :drawioDefaultServerUrl' => '',
-];
+return array (
+  '<strong>Create</strong> draw.io diagram' => '<strong>draw.io</strong> diagram létrehozása',
+  '<strong>Draw.io </strong> module configuration' => '<strong>Draw.io </strong> modul konfiguráció',
+  'Could not find requested file!' => 'Nem található a kért fájl!',
+  'Create draw.io document' => 'draw.io dokumentum létrehozása',
+  'Edit using draw.io' => 'Szerkesztés draw.io-val.',
+  'File write access denied!' => 'A fájl írása megtagadva!',
+  'Hostname' => 'Hosztnév',
+  'Open the new document in the next step' => 'Nyissa meg az új dokumentumot következő lépésként',
+  'e.g. :drawioDefaultServerUrl' => 'Pl. :drawioDefaultServerUrl',
+);
