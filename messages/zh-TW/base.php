620c42a5ecc2ab1d7da97ce08e15f83603bc8187
HumHub Translations <hello@humhub.com>
2023-03-01 18:06:27 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/zh-TW/base.php
+++ messages/zh-TW/base.php
@@ -2,7 +2,7 @@
 return array (
   '<strong>Create</strong> draw.io diagram' => '',
   '<strong>Draw.io </strong> module configuration' => '',
-  'Could not find requested file!' => '',
+  'Could not find requested file!' => '無法找到要求的文件!',
   'Create draw.io document' => '',
   'Edit using draw.io' => '',
   'File write access denied!' => '',
