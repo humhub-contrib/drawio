eee73531de5101fad0fa9df51ed4f0d85ffe83d7
HumHub Translations <hello@humhub.com>
2023-05-03 16:59:35 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/ja/base.php
+++ messages/ja/base.php
@@ -2,10 +2,10 @@
 return array (
   '<strong>Create</strong> draw.io diagram' => 'draw.io ダイアグラムを <strong>作成</strong>',
   '<strong>Draw.io </strong> module configuration' => '<strong>Draw.io </strong> モジュールの構成',
-  'Could not find requested file!' => 'お探しのファイルが見つかりませんでした',
+  'Could not find requested file!' => 'お探しのファイルが見つかりませんでした！',
   'Create draw.io document' => 'draw.io ドキュメントを作成する',
   'Edit using draw.io' => 'draw.io を使用して編集する',
-  'File write access denied!' => 'ファイルへの書込が拒否されました',
+  'File write access denied!' => 'ファイルへの書込が拒否されました！',
   'Hostname' => 'ホスト名',
   'Open the new document in the next step' => '次のステップで新しいドキュメントを開きます',
   'e.g. :drawioDefaultServerUrl' => '例：drawioDefaultServerUrl',
