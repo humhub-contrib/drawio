4f2d300e5c5c062148c487dec7c1dea2cf1ea189
HumHub Translations <hello@humhub.com>
2023-07-12 16:22:16 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/ko/base.php
+++ messages/ko/base.php
@@ -1,13 +1,12 @@
 <?php
-
-return [
-    '<strong>Create</strong> draw.io diagram' => '',
-    '<strong>Draw.io </strong> module configuration' => '',
-    'Could not find requested file!' => '',
-    'Create draw.io document' => '',
-    'Edit using draw.io' => '',
-    'File write access denied!' => '',
-    'Hostname' => '',
-    'Open the new document in the next step' => '',
-    'e.g. :drawioDefaultServerUrl' => '',
-];
+return array (
+  '<strong>Create</strong> draw.io diagram' => '',
+  '<strong>Draw.io </strong> module configuration' => '',
+  'Could not find requested file!' => '요청한 파일을 찾을 수 없습니다!',
+  'Create draw.io document' => '',
+  'Edit using draw.io' => '',
+  'File write access denied!' => '',
+  'Hostname' => '',
+  'Open the new document in the next step' => '',
+  'e.g. :drawioDefaultServerUrl' => '',
+);
