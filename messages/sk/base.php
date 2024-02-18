742418ee0da6f8a5847a342d496c8d0a45cc2934
HumHub Translations <hello@humhub.com>
2024-02-18 18:19:22 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/sk/base.php
+++ messages/sk/base.php
@@ -1,13 +1,12 @@
 <?php
-
-return [
-    '<strong>Create</strong> draw.io diagram' => '',
-    '<strong>Draw.io </strong> module configuration' => '',
-    'Could not find requested file!' => '',
-    'Create draw.io document' => '',
-    'Edit using draw.io' => '',
-    'File write access denied!' => '',
-    'Hostname' => '',
-    'Open the new document in the next step' => '',
-    'e.g. :drawioDefaultServerUrl' => '',
-];
+return array (
+  '<strong>Create</strong> draw.io diagram' => '',
+  '<strong>Draw.io </strong> module configuration' => '',
+  'Could not find requested file!' => 'Požadovaný súbor sa nepodarilo nájsť!',
+  'Create draw.io document' => '',
+  'Edit using draw.io' => '',
+  'File write access denied!' => '',
+  'Hostname' => '',
+  'Open the new document in the next step' => '',
+  'e.g. :drawioDefaultServerUrl' => '',
+);
--- messages/sr/base.php
+++ messages/sr/base.php
@@ -1,13 +1,12 @@
 <?php
-
-return [
-    '<strong>Create</strong> draw.io diagram' => '',
-    '<strong>Draw.io </strong> module configuration' => '',
-    'Could not find requested file!' => '',
-    'Create draw.io document' => '',
-    'Edit using draw.io' => '',
-    'File write access denied!' => '',
-    'Hostname' => '',
-    'Open the new document in the next step' => '',
-    'e.g. :drawioDefaultServerUrl' => '',
-];
+return array (
+  '<strong>Create</strong> draw.io diagram' => '<strong>Kreiraj</strong> draw.io diagram',
+  '<strong>Draw.io </strong> module configuration' => '<strong>Draw.io </strong> konfiguracija modula',
+  'Could not find requested file!' => 'Nije pronadjen zahtevan fajl',
+  'Create draw.io document' => 'Kreiraj draw.io dokument',
+  'Edit using draw.io' => 'Uredi koristeći draw.io',
+  'File write access denied!' => 'Pristup za upisivanje fajla odbijen!',
+  'Hostname' => 'Hostname',
+  'Open the new document in the next step' => 'Otvorite novi dokument u sledecem koraku',
+  'e.g. :drawioDefaultServerUrl' => '',
+);
