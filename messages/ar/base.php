ff511acf133dea7327511d9cbebdfb60d97f72e6
HumHub Translations <hello@humhub.com>
2024-08-28 14:59:59 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/ar/base.php
+++ messages/ar/base.php
@@ -1,13 +1,12 @@
 <?php
-
-return [
-    'Could not find requested file!' => 'لم يتم ايجاد الملف المطلوب',
-    'Hostname' => '@Hostname@',
-    '<strong>Create</strong> draw.io diagram' => '',
-    '<strong>Draw.io </strong> module configuration' => '',
-    'Create draw.io document' => '',
-    'Edit using draw.io' => '',
-    'File write access denied!' => '',
-    'Open the new document in the next step' => '',
-    'e.g. :drawioDefaultServerUrl' => '',
-];
+return array (
+  '<strong>Create</strong> draw.io diagram' => '<strong>إنشاء</strong> مخطط draw.io',
+  '<strong>Draw.io </strong> module configuration' => 'إعداد موديول <strong>Draw.io </strong>',
+  'Could not find requested file!' => 'لم يتم ايجاد الملف المطلوب',
+  'Create draw.io document' => 'إنشاء مستند draw.io',
+  'Edit using draw.io' => 'تحرير باستخدام draw.io',
+  'File write access denied!' => 'تم رفض الوصول للكتابة على الملف!',
+  'Hostname' => 'اسم المضيف',
+  'Open the new document in the next step' => 'افتح المستند الجديد في الخطوة التالية',
+  'e.g. :drawioDefaultServerUrl' => 'مثال :drawioDefaultServerUrl',
+);
